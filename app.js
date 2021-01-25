a09494028239f1633c369995c6b8eee248b042f0
Ijas mohammed <ijas.mi2714@gmail.com>
2021-01-25 11:16:51 +0530

    ex(01-04)

--- app.js
+++ app.js
@@ -2,11 +2,18 @@
 // prompt("enter your name")
 
 var btnClk=document.querySelector("#btn");
+var textArea=document.querySelector("#text");
 console.log(btnClk)
+
 
 btnClk.addEventListener("click", clickhandler)
 
 function clickhandler()
 {
+    
     console.log("clicked");
+    console.log(textArea.value);
+
 }
+
+
